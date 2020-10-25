eaf066878e9400b46e898fc7e90748bc6352cad8
LehaGer <leha.ger97@mail.ru>
2020-10-25 20:21:37 +0300

    feat: implement reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    
+    let ResultStr = "";
+
+    n = Math.abs(n)
+
+    for(let i = n.toString().length-1; i >= 0; i--){
+        ResultStr += n.toString().charAt(i);
+    }
+
+    return ResultStr;
 }
